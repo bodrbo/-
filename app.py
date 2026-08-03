436426c179d044fae82ba82389e1f77f7176e450
bodrbo <info@bodrbo-refit.com>
2026-08-03 11:52:45 +0300

    Add team login for Арсений Коннов (konnov)

--- app.py
+++ app.py
@@ -260,6 +260,8 @@ TEAM_ACCOUNTS = [
      "pbkdf2:sha256:1000000$SISeExEgbIkK4mHV$39df88e3c4dd43c33ac6a7f6616505bb7c8dbef392862c3664607862cc33b5c4"),
     ("Марина Кащенко", "Kashenko",
      "pbkdf2:sha256:1000000$Jn5CnA4kS1II0z13$ee1913ce0b9ebf6d831d5c4b20e9fe1fe83dc86a952964a111c3ce2a176a069f"),
+    ("Арсений Коннов", "konnov",
+     "pbkdf2:sha256:1000000$K1cAmlBOoffmz5Yh$7264379b419cfdb8200d6ef478eea14f70d5861090841c94899fade88477388d"),
 ]
 
 SALE_CHANNELS = [
